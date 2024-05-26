7247ff889a3788cefebf7203f6eac37bb236382c
nccucs11017 <101815460+nccucs11017@users.noreply.github.com>
2024-05-26 22:32:58 +0800

    update news ajax

---
+++ 股票/src/views/data/fake_news.js
@@ -0,0 +1,74 @@
+export const newsArray = [
+  {
+    "title": "520前死守兩萬！三大法人賣超261.59億元…大盤竟反攻 網嚇壞：政府打銀彈進來啦",
+    "url": "https://tw.stock.yahoo.com/news/520前死守兩萬-三大法人賣超261-59億元-大盤竟反攻-網嚇壞-083000632.html",
+    "imgurl": "https://media.zenfs.com/en/ftnn_com_tw_939/9fe1ee7e1e721c0278f2537922b24e3f",
+    "time": "2024 年 4 月 17 日"
+  },
+  {
+    "title": "外資連六賣 賣超222億元鎖定面板雙虎提款 逆勢狂掃大同5.1萬張",
+    "url": "https://m.cnyes.com/news/id/5528121",
+    "imgurl": "https://cimg.cnyes.cool/prod/news/5528121/l/b9707e1313b96132518de0d4429955ec.jpg",
+    "time": "2024 年 4 月 17 日"
+  },
+  {
+    "title": "M31 3月較去年同期轉虧 每股虧0.1元",
+    "url": "https://m.cnyes.com/news/id/5528138",
+    "imgurl": "https://cimg.cnyes.cool/prod/news/5528138/l/4ab3cea13255b7bc1cad6ecf108e0d9c.jpg",
+    "time": "2024 年 4 月 17 日"
+  },
+  {
+    "title": "520前死守兩萬！三大法人賣超261.59億元…大盤竟反攻 網嚇壞：政府打銀彈進來啦",
+    "url": "https://tw.stock.yahoo.com/news/520前死守兩萬-三大法人賣超261-59億元-大盤竟反攻-網嚇壞-083000632.html",
+    "imgurl": "https://media.zenfs.com/en/ftnn_com_tw_939/9fe1ee7e1e721c0278f2537922b24e3f",
+    "time": "2024 年 4 月 17 日"
+  },
+  {
+    "title": "外資連六賣 賣超222億元鎖定面板雙虎提款 逆勢狂掃大同5.1萬張",
+    "url": "https://m.cnyes.com/news/id/5528121",
+    "imgurl": "https://cimg.cnyes.cool/prod/news/5528121/l/b9707e1313b96132518de0d4429955ec.jpg",
+    "time": "2024 年 4 月 17 日"
+  },
+  {
+    "title": "M31 3月較去年同期轉虧 每股虧0.1元",
+    "url": "https://m.cnyes.com/news/id/5528138",
+    "imgurl": "https://cimg.cnyes.cool/prod/news/5528138/l/4ab3cea13255b7bc1cad6ecf108e0d9c.jpg",
+    "time": "2024 年 4 月 17 日"
+  },
+  {
+    "title": "520前死守兩萬！三大法人賣超261.59億元…大盤竟反攻 網嚇壞：政府打銀彈進來啦",
+    "url": "https://tw.stock.yahoo.com/news/520前死守兩萬-三大法人賣超261-59億元-大盤竟反攻-網嚇壞-083000632.html",
+    "imgurl": "https://media.zenfs.com/en/ftnn_com_tw_939/9fe1ee7e1e721c0278f2537922b24e3f",
+    "time": "2024 年 4 月 17 日"
+  },
+  {
+    "title": "外資連六賣 賣超222億元鎖定面板雙虎提款 逆勢狂掃大同5.1萬張",
+    "url": "https://m.cnyes.com/news/id/5528121",
+    "imgurl": "https://cimg.cnyes.cool/prod/news/5528121/l/b9707e1313b96132518de0d4429955ec.jpg",
+    "time": "2024 年 4 月 17 日"
+  },
+  {
+    "title": "M31 3月較去年同期轉虧 每股虧0.1元",
+    "url": "https://m.cnyes.com/news/id/5528138",
+    "imgurl": "https://cimg.cnyes.cool/prod/news/5528138/l/4ab3cea13255b7bc1cad6ecf108e0d9c.jpg",
+    "time": "2024 年 4 月 17 日"
+  },
+  {
+    "title": "520前死守兩萬！三大法人賣超261.59億元…大盤竟反攻 網嚇壞：政府打銀彈進來啦",
+    "url": "https://tw.stock.yahoo.com/news/520前死守兩萬-三大法人賣超261-59億元-大盤竟反攻-網嚇壞-083000632.html",
+    "imgurl": "https://media.zenfs.com/en/ftnn_com_tw_939/9fe1ee7e1e721c0278f2537922b24e3f",
+    "time": "2024 年 4 月 17 日"
+  },
+  {
+    "title": "外資連六賣 賣超222億元鎖定面板雙虎提款 逆勢狂掃大同5.1萬張",
+    "url": "https://m.cnyes.com/news/id/5528121",
+    "imgurl": "https://cimg.cnyes.cool/prod/news/5528121/l/b9707e1313b96132518de0d4429955ec.jpg",
+    "time": "2024 年 4 月 17 日"
+  },
+  {
+    "title": "M31 3月較去年同期轉虧 每股虧0.1元",
+    "url": "https://m.cnyes.com/news/id/5528138",
+    "imgurl": "https://cimg.cnyes.cool/prod/news/5528138/l/4ab3cea13255b7bc1cad6ecf108e0d9c.jpg",
+    "time": "2024 年 4 月 17 日"
+  }
+];
