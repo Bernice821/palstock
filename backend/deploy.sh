e1c5f8d099b9eb425734e6d2449ca45ce59dc454
weichung911 <weichung880911@gmail.com>
2024-06-13 12:47:20 +0800

    modify deploy.sh

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,4 +1,16 @@
 
 echo "Start deploying"
-python main.py
+# python main.py
+
+echo "Starting the Flask app using Gunicorn..."
+gunicorn app:app -b :5000 --daemon
+
+# 确认应用是否启动成功
+if [[ $? -eq 0 ]]; then
+    echo "Application is running."
+else
+    echo "Failed to start the application."
+    exit 1
+fi
+
 echo "Complete!"
